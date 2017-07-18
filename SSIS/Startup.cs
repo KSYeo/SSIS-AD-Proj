312ecfec6725b1a4681019c7298eb5fd3a2638e6
KSYeo <e0167359@u.nus.edu>
2017-07-18 14:22:12 +0800

    Add project files.

---
+++ SSIS/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SSIS.Startup))]
+namespace SSIS
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
